4a0db582080861296f18efa4c716e75ce036ec97
Willie Cohen <wcohen64@gmail.com>
2017-05-03 01:49:55 -0400

    updated test for serial

--- serial/serial_test.sh
+++ serial/serial_test.sh
@@ -1,6 +1,10 @@
-echo Original Game Board
 echo ======================
+#beginning game
 ./play <<< "game b 10 0 0"
-echo Middle Game Board
-echo ======================
+echo "Depth: 10, START"
+#middle game
 ./play -w 0x0002040C1408100 -b 0x0000007008040200 <<< "game b 10 0 0"
+echo "Depth: 10, MIDDLE"
+#Close to end game
+./play -w 0x00101717076F0000 -b 0x0C0C084838103C3C <<< "game b 8 0 0"
+echo "Depth: 8, END"
