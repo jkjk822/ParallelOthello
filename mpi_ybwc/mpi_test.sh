c90f69af394446fc0d7015450bb76796b08694e6
Willie Cohen <wcohen64@gmail.com>
2017-05-05 10:13:37 -0400

    final mpi testing scripts

--- mpi_ybwc/mpi_test.sh
+++ mpi_ybwc/mpi_test.sh
@@ -1,30 +1,33 @@
 N=11
 echo ======================
 beginning game
-for i in 2 4 8 12 16 24 32 48
+for i in 2 3 4 8 12 16 24 32 48
 do
+	echo ==========================================
+	echo "$i processes"
+	echo ==========================================
 	for j in 1 2 3
 	do
 		mpirun -np $i -machinefile hosts ./linux/sor -v <<< "game b $N 0 0"
-		last_pid=$!
-		sleep 10
-		kill -KILL $last_pid
+		# last_pid=$!
+		# sleep 10
+		# kill -KILL $last_pid
 		echo "Depth: $N, START"
 	done
 	for j in 1 2 3
 	do
 		mpirun -np $i -machinefile hosts ./linux/sor -v -w 0x0002040C1408100 -b 0x0000007008040200 <<< "game b $N 0 0"
-		last_pid=$!
-		sleep 10
-		kill -KILL $last_pid
+		# last_pid=$!
+		# sleep 10
+		# kill -KILL $last_pid
 		echo "Depth: $N, MIDDLE"
 	done
-	for j in 1 2 3
-	do
-		mpirun -np $i -machinefile hosts ./linux/sor -v -w 0x00101717076F0000 -b 0x0C0C084838103C3C <<< "game b $N 0 0"
-		last_pid=$!
-		sleep 10
-		kill -KILL $last_pid
-		echo "Depth: $(($N-1)), END"
-	done
+	# for j in 1 2 3
+	# do
+	# 	mpirun -np $i -machinefile hosts ./linux/sor -v -w 0x00101717076F0000 -b 0x0C0C084838103C3C <<< "game b $N 0 0"
+	# 	# last_pid=$!
+	# 	# sleep 10
+	# 	# kill -KILL $last_pid
+	# 	echo "Depth: $(($N-1)), END"
+	# done
 done
