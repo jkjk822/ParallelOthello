88ff23e1a514c76fc2e5c90e636eb821ea1bcb85
Willie Cohen <wcohen64@gmail.com>
2017-05-05 12:26:03 -0400

    parallel testing updates

--- mpi_pvc/mpi_test.sh
+++ mpi_pvc/mpi_test.sh
@@ -1,7 +1,6 @@
 N=11
 echo ======================
-beginning game
-for i in 2 3 4 8 12 16 24 32 48
+for i in 2 3 4 8 12 16 24 32 48 64
 do
 	echo ==========================================
 	echo "$i processes"
--- parallel_ybwc/ybwc_test.sh
+++ parallel_ybwc/ybwc_test.sh
@@ -1,10 +1,32 @@
+N=11
 echo ======================
-#beginning game
-./play -t 40 -v <<< "game b 10 0 0"
-echo "Depth: 10, START"
-#middle game
-./play -t 40 -v -w 0x0002040C1408100 -b 0x0000007008040200 <<< "game b 10 0 0"
-echo "Depth: 10, MIDDLE"
-#Close to end game
-./play -t 40 -v -w 0x00101717076F0000 -b 0x0C0C084838103C3C <<< "game b 8 0 0"
-echo "Depth: 8, END"
+for i in 2 3 4 8 12 16 24 32 48 64 72 128 256
+do
+	echo ==========================================
+	echo "$i processes"
+	echo ==========================================
+	for j in 1 2 3
+	do
+		./play -v -t "$i" <<< "game b $N 0 0"
+		# last_pid=$!
+		# sleep 10
+		# kill -KILL $last_pid
+		echo "Depth: $N, START"
+	done
+	for j in 1 2 3
+	do
+		./play -v -t "$i" -w 0x0002040C1408100 -b 0x0000007008040200 <<< "game b $N 0 0"
+		# last_pid=$!
+		# sleep 10
+		# kill -KILL $last_pid
+		echo "Depth: $N, MIDDLE"
+	done
+	for j in 1 2 3
+	do
+		./play -v -t "$i" -w 0x00101717076F0000 -b 0x0C0C084838103C3C <<< "game b $((N-1)) 0 0"
+		# last_pid=$!
+		# sleep 10
+		# kill -KILL $last_pid
+		echo "Depth: $(($N-1)), END"
+	done
+done
